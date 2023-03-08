382e23ca6355a8cd184cc861713e6d2e588948de
zhoupengfei <zhoupengfei@vguang.cn>
2023-03-08 13:14:11 +0800

    [feat] sysinfo系统信息模块代码实现

--- src/quickjs-sysinfo.c
+++ src/quickjs-sysinfo.c
@@ -14,14 +14,84 @@
 
 
 /**
- * @brief 打开信道
+ * @brief 获取系统启动的运行时间
  */
-static JSValue channelOpen(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+static JSValue sysinfoGetUptime(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
 {
-
-    return JS_UNDEFINED;
+    long uptime = vbar_sysinfo_get_uptime();
+    return JS_NewInt64(ctx, uptime);
 }
 
+/**
+ * @brief 获取系统的总内存
+ */
+static JSValue sysinfoGetTotalmem(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    long totalmem = vbar_sysinfo_get_totalmem();
+    return JS_NewInt64(ctx, totalmem);
+}
+
+/**
+ * @brief 获取系统剩余内存
+ */
+static JSValue sysinfoGetFreemem(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    long freemem = vbar_sysinfo_get_freemem();
+    return JS_NewInt64(ctx, freemem);
+}
+
+/**
+ * @brief 获取系统可用磁盘总量
+ */
+static JSValue sysinfoGetTotaldisk(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    char *path = JS_ToCString(ctx, argv[0]);
+    long totaldisk = vbar_sysinfo_get_totaldisk(path);
+    return JS_NewInt64(ctx, totaldisk);
+}
+
+/**
+ * @brief 获取系统磁盘剩余可用量
+ */
+static JSValue sysinfoGetFreedisk(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    char *path = JS_ToCString(ctx, argv[0]);
+    long freedisk = vbar_sysinfo_get_freedisk(path);
+    return JS_NewInt64(ctx, freedisk);
+}
+
+/**
+ * @brief 获取设备uuid（字符串）
+ */
+static JSValue sysinfoGetUuid(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    char *uuid = JS_ToCString(ctx, argv[0]);
+    uint32_t len;
+    JS_ToInt32(ctx, &len, argv[1]);
+    int res = vbar_sysinfo_get_uuid(uuid, len);
+    return JS_NewInt32(ctx, res);
+}
+
+/**
+ * @brief 获取通过uuid计算的mac地址
+ */
+static JSValue sysinfoGetUuid2mac(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    char *mac = JS_ToCString(ctx, argv[0]);
+    uint32_t len;
+    JS_ToInt32(ctx, &len, argv[1]);
+    int res = vbar_sysinfo_get_uuid2mac(mac, len);
+    return JS_NewInt32(ctx, res);
+}
+
+/**
+ * @brief 获取cpu占用率
+ */
+static JSValue sysinfoGetFreecpu(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+    int freecpu = vbar_sysinfo_get_freecpu();
+    return JS_NewInt32(ctx, freecpu);
+}
 
 
 
@@ -37,7 +107,14 @@ static JSValue sysinfoContructor(
 
 /* 类方法列表填充 */
 static const JSCFunctionListEntry sysinfo_class_funcs[] = {
-    JS_CFUNC_DEF("channelOpen", 2, channelOpen),
+    JS_CFUNC_DEF("sysinfoGetUptime", 0, sysinfoGetUptime),
+    JS_CFUNC_DEF("sysinfoGetTotalmem", 0, sysinfoGetTotalmem),
+    JS_CFUNC_DEF("sysinfoGetFreemem", 0, sysinfoGetFreemem),
+    JS_CFUNC_DEF("sysinfoGetTotaldisk", 1, sysinfoGetTotaldisk),
+    JS_CFUNC_DEF("sysinfoGetFreedisk", 1, sysinfoGetFreedisk),
+    JS_CFUNC_DEF("sysinfoGetUuid", 2, sysinfoGetUuid),
+    JS_CFUNC_DEF("sysinfoGetUuid2mac", 2, sysinfoGetUuid2mac),
+    JS_CFUNC_DEF("vbar_sysinfo_get_freecpu", 0, vbar_sysinfo_get_freecpu),
 };
 
 
